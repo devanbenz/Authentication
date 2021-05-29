fb6b25f1357d9d7e3ba79ee3a9c478d8e47e81b0
devanbenz <devandbenz@gmail.com>
2021-05-28 21:41:40 -0500

    Update content -- not sure what was changed last ;p

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,44 +1,74 @@
-const db = require('../models/mysql_model')
+const {
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PW,
+    MYSQL_DB
+} = require('../config/env')
+const db = require('mysql2/promise')
 const bcrypt = require('bcrypt')
 
-db.connect()
+const dbpool = db.createPool({
+    host: MYSQL_HOST,
+    database: MYSQL_DB,
+    user: MYSQL_USER,
+    password: MYSQL_PW
+})
 
-exports.getUsers = (req, res) => {
-    db.query(`SELECT * FROM users`, (err, result) => {
-        res.status(200).json({
-            status: 'Success',
-            data: result
-        })
-    })
+exports.getUsers = async (req, res) => {
+    try {
+        const [rows] = await dbpool.execute('SELECT * FROM users')
+        console.log(rows[0].username)
+        return res.status(200).json({result:'success'})
+    }
+    catch(err){
+        res.status(400).json({result:'Error'})
+        console.error(err)
+    }
+
 }
 
-exports.userLogin = (req, res) => {
+
+exports.userLogin = async(req, res) => {
     const {username,password} = req.body
-    db.query(`SELECT password FROM users WHERE username = "${username}"`, (err, result) => {
-        bcrypt.compare(password, result[0].password, (err, same) => {
-            res.status(200).json({
-                result: 'Success',
-                data: same
-            })
-        })
-        // res.status(200).json({
-        //     result: 'success',
-        //     data: result[0].password
-        // })
-    })
+    const sqlQ = 'SELECT EXISTS(SELECT * FROM users WHERE username = ?)'
+    const checkpw = 'SELECT * FROM users WHERE username = ? AND password = ?'
+    try {
+        const userInDb = await dbpool.execute(sqlQ, [username])
+        const hashedPw = await bcrypt.hash(password, 10)
 
-}
-
-exports.addUser = (req, res) => {
-    bcrypt.hash(req.body.password, 10, (err, hashedpw) => {
-        if (err) { 
-            console.error(err) 
+        // This is so ugly but mysql2 returns an object with the string as a key -_-
+        // Checks if username doesn't exist
+        if(userInDb[0]['EXISTS(SELECT * FROM users WHERE username = ?)'] == 0) {
+            res.status(401).json({data: 'Username not found'})
             return
         }
-        db.query(`INSERT INTO users (username, password) VALUES ("${req.body.username}","${hashedpw}")`)
-    })
-    res.status(200).json({
-        result: 'success',
-        msg: 'User added to db'
-    })
+        // Username exists! Time to check the password hash
+        const result = await dbpool.execute(checkpw, [username,hashedPw])
+        res.status(200).json({
+            result: 'Success',
+            a: username,
+            b: hashedPw
+        })
+        console.log(result)
+    }
+    catch(err) {
+        res.status(400).json({
+            result: 'Error',
+            msg: err
+        })
+    }
+}
+
+exports.addUser = async (req, res) => {
+    try {
+        const hashedpw = await bcrypt.hash(req.body.password, 10)
+        await dbpool.execute('INSERT INTO users (username, password) VALUES (?,?)',[req.body.username, hashedpw])
+        res.status(200).json({
+            result: 'Success',
+            msg: 'Stored to DB'
+        }) 
+    }
+    catch(err){
+        res.status(400).json({result:'Error', msg: err})
+    }
 }
